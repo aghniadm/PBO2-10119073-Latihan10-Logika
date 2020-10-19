e92cbda5d17c6e7527d9496bbd327cf99f7037ff
aghniadm <aghniadewii@gmail.com>
2020-10-19 08:55:58 +0700

    PBO2-10119073-Latihan10-Logika

---
+++ src/pbo2/pkg10119073/latihan10/logika/PBO210119073Latihan10Logika.java
@@ -0,0 +1,22 @@
+package pbo2.pkg10119073.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan
+ * logika
+ *
+ */
+public class PBO210119073Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
